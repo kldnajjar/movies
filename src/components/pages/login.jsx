3218da164e0baabfad2d9b8a09aa7555e05f8ff7
Khaled <kld.najjar@gmail.com>
2019-07-23 10:28:27 +0300

    Adding login page

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Login extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Login</h1>
+        <form className="mt-4">
+          <div className="form-group">
+            <label htmlFor="usename">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              aria-describedby="userHelp"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="passwordField">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="passwordField"
+              aria-describedby="passwordHelp"
+              placeholder="Enter password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Login;
